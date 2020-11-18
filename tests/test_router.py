b87165017ccd86f7d550b99f35f72f84d00d4d8c
Oleg Medvedev <omdv@fastmail.com>
2020-11-17 22:16:39 -0600

    intermediate progress on isochrone

--- tests/test_router.py
+++ tests/test_router.py
@@ -1,7 +1,7 @@
 """Test routing methods."""
-import unittest
-
+import datetime as dt
 import numpy as np
+import unittest
 
 from windrouter import polars, weather, router
 
@@ -13,17 +13,40 @@ class TestRouterMethods(unittest.TestCase):
         """Testing boat direct method."""
         lats = [0, 0, -40, 0]
         lons = [0, 0, 0, 0]
-        hdgs = [33, 123, 353, 0]
+        hdgs = [33, 123, 353, 188]
+
         boat = polars.boat_properties('data/polar-ITA70.csv')
-        gfs = weather.grib_to_wind_function('data/2019122212/2019122212f000')
-        hours = 1
-        segments = 1
+
+        model = '2020111600'
+        winds = weather.read_wind_functions(model, 24)
+        start_time = dt.datetime.strptime('2020111607', '%Y%m%d%H')
+
         result = router.move_boat_direct(
-            lats, lons, hdgs,
-            boat, gfs,
-            hours, segments,
+            (lats, lons),
+            hdgs,
+            boat, winds,
+            start_time, 3600,
             False
         )
         self.assertTrue(np.allclose(
             result['s12'],
-            [19444.93, 22.315, 30032.68, 17574.56]))
+            [16111.32450562, 21790.40986404, 25748.94679565, 115.94699562]))
+
+    def test_isochrone(self):
+        """Test isochrone."""
+        p1 = ([10, 20, 30, 40], [40, 30, 20, 10])
+        p2 = ([40, 30, 20, 10], [10, 20, 30, 40])
+        params = {
+            'ROUTER_HDGS_INCREMENTS_DEG': 0.5,
+            'ROUTER_HDGS_SEGMENTS': 10,
+            'ROUTER_DELTA_TIME_HRS': 1}
+
+        boat = polars.boat_properties('data/polar-ITA70.csv')
+
+        model = '2020111600'
+        winds = weather.read_wind_functions(model, 24)
+        start_time = dt.datetime.strptime('2020111607', '%Y%m%d%H')
+
+        router.calc_isochrone(
+            p1, p2,
+            boat, winds, start_time, 3600, params, verbose=False)
--- windrouter/router.py
+++ windrouter/router.py
@@ -3,66 +3,116 @@ from geovectorslib import geod
 
 import numpy as np
 
-from .polars import boat_speed_function
+from windrouter.polars import boat_speed_function
+from windrouter.weather import wind_function
 
 
-def move_boat_direct(lats, lons, hdgs,
-                     boat, gfs,
-                     hours, segments,
+def move_boat_direct(start_coords,
+                     hdgs,
+                     boat,
+                     winds,
+                     start_time,
+                     delta_time,
                      verbose=False):
     """
-    Given boat configuration, starting point and route parameters determine
-    the end point.
+    Move boat forward from starting points, given headings and conditions.
 
-    lats, lons, hdgs - vectors of starting points and headings
-    boat - boat profile
-    gfs - interpolation functions for GFS
-    hours, segments - number of hours per segment and number of segments
+            Parameters:
+                start_coords (tuple) - tuple of arrays (lats, lons)
+                hdgs (array) - headings
+                boat (dict) - boat profile
+                winds (dict) - wind functions
+                start_time (datetime) - start time
+                delta_time (float) - time to move in seconds
+
+            Returns:
+                gcr (dict) - dictionary of vectors
     """
-    s12 = np.zeros(len(lats))
+    winds = wind_function(winds, start_coords, start_time)
+    twa = winds['twa']
+    tws = winds['tws']
+    wind = {'tws': tws, 'twa': twa - hdgs}
 
-    p2 = (lats, lons)
-    for i in range(segments):
-        twa = gfs['twa'](p2)
-        tws = gfs['tws'](p2)
-        wind = {'tws': tws, 'twa': twa - hdgs}
+    bs = boat_speed_function(boat, wind)
 
-        bs = boat_speed_function(boat, wind)
+    if verbose:
+        print('TWA: ', twa)
+        print('TWS: ', tws)
+        print('HDG: ', hdgs)
+        print('WA:  ', twa - hdgs)
+        print('BS:  ', bs)
 
-        if verbose:
-            print('TWA: ', twa)
-            print('TWS: ', tws)
-            print('HDG: ', hdgs)
-            print('WA:  ', twa - hdgs)
-            print('BS:  ', bs)
+    # distance in meters
+    dist = delta_time * bs
 
-        # distance in meters
-        dist = (hours * 3600) * bs
+    # move boat
+    gcr = geod.direct(start_coords[0], start_coords[1], hdgs, dist)
 
-        # move boat
-        g = geod.direct(p2[0], p2[1], hdgs, dist)
-
-        s12 += dist
-        p2 = (g['lat2'], g['lon2'])
-
-    return {'azi1': hdgs, 's12': dist, 'p2': p2, 't12': hours * segments}
+    return {'azi1': hdgs,
+            's12': dist,
+            'p2': (gcr['lat2'], gcr['lon2']),
+            't12': delta_time}
 
 
-def calc_isochrone(start, boat, gfs):
+def calc_isochrone(start_coords,
+                   end_coords,
+                   boat,
+                   winds,
+                   start_time,
+                   delta_time,
+                   params,
+                   verbose=False):
     """
-    Estimate isochrones given a variety of headings.
+    Get one isochrone with pruning.
 
-    start - scalar start point
-    boat - boat profile
-    gfs - interpolation functions for interpolated GFS
+            Parameters:
+                start_coords (tuple) - tuple of arrays (lats, lons)
+                end_coords (tuple) - tuple of arrays (lats, lons)
+                boat (dict) - boat profile
+                winds (dict) - wind functions
+                start_time (datetime) - start time
+                delta_time (float) - time to move in seconds
+                params (dict) - isochrone calculation parameters
+
+            Returns:
+                gcr (dict) - dictionary of array of isochrones
     """
-    hdgs = [10 * i for i in range(36)]
-    hours = 10
-    segments = 1
-    lat = start[0]
-    lon = start[1]
-    iso = move_boat_direct(
-        lat * np.ones(len(hdgs)),
-        lon * np.ones(len(hdgs)),
-        hdgs, boat, gfs, hours, segments, True)
-    return iso
+    # determine headings towards end point
+    gcrs = geod.inverse(start_coords[0],
+                        start_coords[1],
+                        end_coords[0],
+                        end_coords[1])
+
+    # construct parameters for boat_direct call
+    lats = np.repeat(start_coords[0], params['ROUTER_HDGS_SEGMENTS'] + 1)
+    lons = np.repeat(start_coords[0], params['ROUTER_HDGS_SEGMENTS'] + 1)
+
+    # vector of GCR heading +/- increments
+    hdgs = np.repeat(gcrs['azi1'], params['ROUTER_HDGS_SEGMENTS'] + 1)
+    delta_hdgs = np.linspace(
+        -params['ROUTER_HDGS_SEGMENTS'] * params['ROUTER_HDGS_INCREMENTS_DEG'],
+        +params['ROUTER_HDGS_SEGMENTS'] * params['ROUTER_HDGS_INCREMENTS_DEG'],
+        params['ROUTER_HDGS_SEGMENTS'] + 1)
+    delta_hdgs = np.tile(delta_hdgs, len(start_coords[0]))
+    hdgs = hdgs - delta_hdgs
+
+    # call boat direct with N_coords x (ROUTER_HDGS_SEGMENTS+1) vector
+    iso = move_boat_direct((lats, lons),
+                           hdgs,
+                           boat,
+                           winds,
+                           start_time,
+                           delta_time,
+                           verbose=False)
+
+    # hdgs = [10 * i for i in range(36)]
+    # hours = 10
+    # segments = 1
+    # lat = start[0]
+    # lon = start[1]
+    # iso = move_boat_direct(
+    #     lat * np.ones(len(hdgs)),
+    #     lon * np.ones(len(hdgs)),
+    #     hdgs, boat, gfs, hours, segments, True)
+    print(iso)
+    return None
